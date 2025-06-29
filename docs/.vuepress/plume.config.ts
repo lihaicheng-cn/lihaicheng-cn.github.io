a8c625b52a42561b79c7eec0d14df3db52745dc2
lihaicheng <lihaicheng.email@gmail.com>
2025-06-29 22:51:50 +0800

    fix: correction of Twitter social links

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -12,7 +12,7 @@ export default defineThemeConfig({
 
   social: [
     { icon: 'github', link: 'https://github.com/lihaicheng-cn' },
-    { icon: 'twitter', link: 'https://twitter.com/lihaicheng-cn' },
+    { icon: 'twitter', link: 'https://twitter.com/lihaicheng_cn' },
   ],
   navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
